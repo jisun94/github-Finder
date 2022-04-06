c80e0e8bbf568fe687ee70d0544590ef109a581e
jisun94 <jisun94lee@gmail.com>
2022-04-07 01:28:57 +0900

    🚧 Connect pages using Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
@@ -7,7 +10,14 @@ function App() {
     <Router>
       <div className="flex flex-col justify-between h-screen ">
         <Navbar />
-        <main>Content</main>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
