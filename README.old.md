d665bd94437df88570750ab89c0a1888e60445c8
jisun94 <jisun94lee@gmail.com>
2022-04-06 16:44:00 +0900

    🎉 Introduce a new project - Github Finder

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# github-finder-2022
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-green-100">
+      <h1 className="text-xl">hello</h1>
+    </div>
+  );
+}
+
+export default App;
