fe36fae769710ca5c97de8c0f0370bc79cd41d37
jisun94 <jisun94lee@gmail.com>
2022-04-07 01:16:17 +0900

    🐛 Fix a wrong route of Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
