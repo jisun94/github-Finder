873bc9d74fc9afa7a36fc8af47c6cabc4646e388
jisun94 <jisun94lee@gmail.com>
2022-04-07 20:49:07 +0900

    ✨ Search users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -12,16 +12,21 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  //Get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`);
 
-    const data = await response.json();
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const { items } = await response.json();
 
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
 
@@ -34,7 +39,7 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
       }}
     >
       {children}
